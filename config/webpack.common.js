298a25b963247672a2461cc7e671d7b742f9f3b5
targeral <2654955040@qq.com>
2017-01-09 16:00:04 +0800

    测试，调试环境

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -25,17 +25,17 @@ module.exports = {
         loader: 'html'
       },
       {
-        test: /\.(png|jpe?g|svg|woff|woff2|ttf|eot|ico)$/,
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
         loader: 'file?name=assets/[name].[hash].[ext]'
       },
       {
         test: /\.css$/,
-        exclude: helpers.root(''),
+        exclude: helpers.root('src', 'app'),
         loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
       },
       {
         test: /\.css$/,
-        include: helpers.root(),
+        include: helpers.root('src', 'app'),
         loader: 'raw'
       }
     ]
